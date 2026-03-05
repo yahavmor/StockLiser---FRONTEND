55ded527f866167f0d1c2989ff6419017f286e3c
yahav mor <yahavmor77@gmail.com>
2026-03-05 16:48:13 +0200

    building header

---
+++ src/cmps/header.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+export function Header(){
+    return(
+        <>
+        <nav className="nav-bar">
+            <ul className="nav-info">
+                <li>Home</li>
+                <li>Stats</li>
+                <li>Login</li>
+                <li>User</li>
+            </ul>
+        </nav>
+        </>
+    )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { RootCmp } from './RootCmp'
 import { BrowserRouter } from 'react-router-dom'
 import './assets/styles/main.css'
+import { Header } from './cmps/header'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	  <BrowserRouter>
+	 		<Header/> 
 			<RootCmp />
 	  </BrowserRouter>
 )
