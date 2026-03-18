253932fd628657bcc9f73ac64932962c2e503c35
yahavmor <yahavmor77@gmail.com>
2026-03-18 15:23:09 +0200

    styling home page and header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,21 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
-export function HomePage(){
-    return(
-        <h1>Home Page</h1>
+export function HomePage() {
+    const navigate = useNavigate()
+
+    return (
+        <div className="home-page">
+            <div className="overlay">
+                <h1 className="title">Welcome to MemeMarket</h1>
+                <p className="subtitle">Explore stocks, save memes, and manage everything in one place</p>
+
+                <div className="btn-container">
+                    <button onClick={() => navigate("/login")}>Login</button>
+                    <button onClick={() => navigate("/meme")}>Random Meme</button>
+                    <button onClick={() => navigate("/user")}>My Profile</button>
+                </div>
+            </div>
+        </div>
     )
 }
